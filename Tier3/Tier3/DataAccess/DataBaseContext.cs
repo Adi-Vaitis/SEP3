14723b1a7672dafca32d6cc12b74d6dc31785306
StapanulLumii <sandu.malai@mail.ru>
2021-11-29 09:42:33 +0100

    Implementing the context class

--- Tier3/Tier3/DataAccess/DataBaseContext.cs
+++ Tier3/Tier3/DataAccess/DataBaseContext.cs
@@ -1,7 +1,24 @@
-﻿namespace Tier3.DataAccess
+﻿using Microsoft.EntityFrameworkCore;
+using Tier3.Models;
+using Tier3.Models.Burial;
+using Tier3.Models.Client;
+using Tier3.Models.Employee;
+using Tier3.Models.Preference;
+
+namespace Tier3.DataAccess
 {
     public class DataBaseContext
     {
+        
+        public DbSet<Burial> Burial { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Preference> Preferences { get; set; }
+        public DbSet<BurialPreference> BurialPreferences { get; set; }
+        public DbSet<ClientBurial> ClientBurials { get; set; }
+        public DbSet<ClientPreference> ClientPreferences { get; set; }
+        
+        
         
     }
 }
