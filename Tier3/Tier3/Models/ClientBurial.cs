00c327a3d21d1e5914e91890b08ff6fcd0678473
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-11-29 09:07:59 +0100

    Model classes done

---
+++ Tier3/Tier3/Models/ClientBurial.cs
@@ -0,0 +1,10 @@
+﻿namespace Tier3.Models
+{
+    public class ClientBurial
+    {
+        public int ClientId { get; set; }
+        public Client.Client Client { get; set; }
+        public int BurialId { get; set; }
+        public Burial.Burial BurialClient { get; set; }
+    }
+}
---
+++ Tier3/Tier3/Models/ClientPreference.cs
@@ -0,0 +1,10 @@
+﻿namespace Tier3.Models
+{
+    public class ClientPreference
+    {
+        public int ClientId { get; set; }
+        public Client.Client Client { get; set; }
+        public int PreferenceId { get; set; }
+        public Preference.Preference PreferenceClient { get; set; }
+    }
+}
