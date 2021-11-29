7d9b78c46f7d6bc050bceba0ec9a06e25992fed4
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-11-29 13:34:26 +0100

    implemented service class

---
+++ Tier 1/Tier 1/Data/ClientService/ClientService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Tier_1.Models.Client;
+
+namespace Tier_1.Data.ClientService
+{
+    public class ClientService : IClientService
+    {
+        public Task<string> CreateClientAccount(Client client)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Client> GetClient(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Client> ValidateClient(Client client)
+        {
+            HttpClient httpClient = new HttpClient();
+            string clientSerialized = JsonSerializer.Serialize(client);
+            
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://localhost:8080/login"),
+                Content = new StringContent(clientSerialized, Encoding.UTF8, "application/json")
+            };
+
+            var response = httpClient.SendAsync(request).ConfigureAwait(false);
+            var responseInfo = response.GetAwaiter().GetResult();
+            string s = await responseInfo.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Client>(s);
+        }
+
+        public Task DeleteClient(int clientId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> EditClientAccount(Client client)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Client> GetClientByUsername(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Client> GetClientById(int clientId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<Client>> GetBurialsClient(int clientId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task AddPreferenceToBurial(int burialId, int preferenceId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletePreferenceFromBurial(int burialId, int preferenceId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Tier 1/Tier 1/Data/ClientService/IClientService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tier_1.Models.Client;
+
+namespace Tier_1.Data.ClientService
+{
+    public interface IClientService
+    {
+        Task<string> CreateClientAccount(Models.Client.Client client);
+        Task<Models.Client.Client> GetClient(string username, string password);
+        Task<Client> ValidateClient(Client client);
+        Task DeleteClient(int clientId);
+        Task<string> EditClientAccount(Models.Client.Client client);
+        Task<Models.Client.Client> GetClientByUsername(string username);
+        Task<Models.Client.Client> GetClientById(int clientId);
+        Task<IList<Models.Client.Client>> GetBurialsClient(int clientId);
+        Task AddPreferenceToBurial(int burialId, int preferenceId);
+        Task DeletePreferenceFromBurial(int burialId, int preferenceId);
+    }
+}
