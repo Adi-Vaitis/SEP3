07f43bdade592c41258bb1854b6394635445b998
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-12-02 00:18:45 +0100

    implememting the preference repo class

---
+++ Tier3/Tier3/Repositories/Preference/PreferenceRepo.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace Tier3.Repositories.Preference
+{
+    public class PreferenceRepo : IPreferenceRepo
+    {
+        public Task<string> CreatePreference(Models.Preference.Preference preference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Models.Preference.Preference> GetPreference(int preferenceId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> EditPreference(Models.Preference.Preference preference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletePreference(int preferenceId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Models.Preference.Preference> GetPreferenceById(int preferenceId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Models.Preference.Preference> GetPreferenceByBurialId(int burialId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
