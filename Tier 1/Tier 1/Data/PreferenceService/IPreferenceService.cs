a7134a2bf71f8e2571ca50bf7ef1913ea10c2c34
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-12-02 10:20:02 +0100

    PreferenceService.cs class

---
+++ Tier 1/Tier 1/Data/PreferenceService/IPreferenceService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tier_1.Models.Preference;
+
+namespace Tier_1.Data.PreferenceService
+{
+    public interface IPreferenceService
+    {
+        Task<IList<Preference>> GetPreferencesAsync();
+        Task AddPreference(Preference preference);
+    }
+}
---
+++ Tier 1/Tier 1/Data/PreferenceService/PreferenceService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tier_1.Models.Preference;
+
+namespace Tier_1.Data.PreferenceService
+{
+    public class PreferenceService : IPreferenceService
+    {
+        public Task<IList<Preference>> GetPreferencesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task AddPreference(Preference preference)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
