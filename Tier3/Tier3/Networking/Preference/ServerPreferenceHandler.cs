7e303c2cd56291576fe6a5b6a607c7c286a7a4af
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-12-02 01:16:24 +0100

    get preference method

---
+++ Tier3/Tier3/Networking/Preference/ServerPreferenceHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using Tier3.Repositories.Preference;
+
+namespace Tier3.Networking.Preference
+{
+    public class ServerPreferenceHandler
+    {
+        private IList<Models.Preference.Preference> preferences;
+        private IPreferenceRepo preferenceRepo;
+
+        public ServerPreferenceHandler()
+        {
+            preferences = new List<Models.Preference.Preference>();
+            preferenceRepo = new PreferenceRepo();
+        }
+
+        public async void GetPreferences(NetworkStream stream)
+        {
+            preferences = await preferenceRepo.GetPreference();
+            string reply = JsonSerializer.Serialize(preferences);
+            byte[] bytesWrite = Encoding.ASCII.GetBytes(reply);
+            stream.Write(bytesWrite, 0, bytesWrite.Length);
+        }
+        
+        
+    }
+}
