c4b2b1e00449045433f6c39ed201725e391b236b
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-12-02 10:08:01 +0100

    working on tier 1

---
+++ Tier 1/Tier 1/Data/BurialService/BurialService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tier_1.Models.Burial;
+
+namespace Tier_1.Data.BurialService
+{
+    public class BurialService : IBurialService
+    {
+        public Task<IList<Burial>> GetBurialsAsync(int clientId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task CreateBurial(Burial burial)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task EditBurial(Burial burial)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteBurial(int burialId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
