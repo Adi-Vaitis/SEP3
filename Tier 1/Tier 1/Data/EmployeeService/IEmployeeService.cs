12c15d397627353aa22a4e37b98a48dd4b64f7ca
Adrian-Gabriel Vaitis <adriangabriel344@gmail.com>
2021-12-09 11:44:22 +0100

    tier 1 employee

---
+++ Tier 1/Tier 1/Data/EmployeeService/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Tier_1.Data.EmployeeService
+{
+    public interface IEmployeeService
+    {
+        Task<Models.Employee.Employee> GetEmployee(string username, string password);
+        Task DeleteEmployee(int employeeId);
+        Task<Models.Employee.Employee> GetEmployeeByUsername(string username);
+        Task<Models.Employee.Employee> GetEmployeeById(int employeeId);
+        Task DeleteBurial(int burialId);
+        Task<Models.Burial.Burial> GetBurial(int burialId);
+    }
+}
